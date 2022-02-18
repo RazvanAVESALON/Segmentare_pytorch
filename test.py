0b763ce6f4353f3bd818c74174d625bbe7576d9e
Razvan Avesalon <89531556+RazvanAVESALON@users.noreply.github.com>
2022-02-18 12:12:28 +0200

    Update test.py

--- test.py
+++ test.py
@@ -16,10 +16,22 @@ from UNetModel import UNet
 from lungs_class import LungSegDataset
 from configurare_data import create_dataset_csv , split_dataset
 from test_function import test
+from datetime import datetime
+import os 
+
+from lungs_class import plot_acc_loss
 config = None
 with open('config.yaml') as f: # reads .yml/.yaml files
     config = yaml.safe_load(f)
     
+yml_data=yaml.dump(config)
+directory =f"Test{datetime.now().strftime('%m%d%Y_%H%M')}"
+parent_dir =r'D:\ai intro\Pytorch\Segmentare_pytorch\Experiment02172022_1831'
+path = os.path.join(parent_dir, directory)
+os.mkdir(path)
+
+f= open(f"{path}\\yaml_config.txt","w+")
+f.write(yml_data)    
 
 dataset_df = create_dataset_csv(config["data"]["images_dir"], 
                                 config["data"]["right_masks_dir"],
@@ -33,7 +45,7 @@ test_df = dataset_df.loc[dataset_df["subset"] == "test", :]
 test_ds = LungSegDataset(test_df, img_size=config["data"]["img_size"])
 test_loader = torch.utils.data.DataLoader(test_ds, batch_size=config["train"]["bs"], shuffle=False)
 
-network = torch.load(r"D:\\ai intro\\Pytorch\\Segmentare_pytorch\\my_model.pth")
+network = torch.load(r"D:\ai intro\Pytorch\Segmentare_pytorch\Experiment02172022_1831\Weights\my_model02172022_1926.pt")
 
 print(f"# Test: {len(test_ds)}")
 
@@ -67,5 +79,8 @@ for i, (img, gt, pred) in enumerate(zip(x[:nr_exs], y[:nr_exs], y_pred[:nr_exs])
     axs[i][2].set_title('Prediction')
     axs[i][2].imshow(pred, cmap='gray')
     
-plt.show()        
+plt.savefig(f"{path}\\Măști")     
+
+
+       
         
